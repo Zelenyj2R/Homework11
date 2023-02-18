89a2036594f12a923ec3f1b00cfb8b401f92b4e6
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-02-18 19:35:49 +0100

    Update OddNames.java

--- src/main/java/ListOfNames/OddNames.java
+++ src/main/java/ListOfNames/OddNames.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class OddNames {
-    public static String OddNamesOutput(List<String> names) {
+    public static String oddNamesOutput(List<String> names) {
         return IntStream.range(0, names.size())
                 .filter(i -> i % 2 != 0)
                 .mapToObj(i -> i + ". " + names.get(i))
