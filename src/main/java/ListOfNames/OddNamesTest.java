78b442aaeb5afd5de11271a5962f47ab30048ac1
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-02-18 19:34:03 +0100

    Update OddNamesTest.java

--- src/main/java/ListOfNames/OddNamesTest.java
+++ src/main/java/ListOfNames/OddNamesTest.java
@@ -6,6 +6,6 @@ public class OddNamesTest {
     public static List<String> names = List.of("Ivan", "Hans", "Peter", "Elke", "Magda", "Kristian");
 
     public static void main(String[] args) {
-        System.out.println(OddNames.OddNamesOutput(names));
+        System.out.println(OddNames.oddNamesOutput(names));
     }
 }
