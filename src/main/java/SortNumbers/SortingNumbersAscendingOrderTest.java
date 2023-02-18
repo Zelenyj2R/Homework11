b2f45a027c6cb0aab3df238ac73d2eccb6e62812
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-02-18 19:32:46 +0100

    Update SortingNumbersAscendingOrderTest.java

--- src/main/java/SortNumbers/SortingNumbersAscendingOrderTest.java
+++ src/main/java/SortNumbers/SortingNumbersAscendingOrderTest.java
@@ -3,7 +3,7 @@ package SortNumbers;
 public class SortingNumbersAscendingOrderTest {
     public static void main(String[] args) {
         String [] arr = {"1, 2, 0", "4, 5"};
-        System.out.println(SortingNumbersAscendingOrder.SortingNumbersMethod(arr));
+        System.out.println(SortingNumbersAscendingOrder.sortingNumbersMethod(arr));
 
     }
 }
